c0a60726636d9fb080920b5a5a37b3fba39cc629
Jesse Wolgamott <jesse_wolgamott@eogresources.com>
2019-10-25 08:43:16 -0500

    Add typescript version

--- src/components/NowWhat.tsx
+++ src/components/NowWhat.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardHeader from "./CardHeader";
-import Typography from "@material-ui/core/Typography";
-import CardContent from "@material-ui/core/CardContent";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { makeStyles } from "@material-ui/core/styles";
-import Avatar from "./Avatar";
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from './CardHeader';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from './Avatar';
 
 const useStyles = makeStyles({
   card: {
-    margin: "5% 25%"
-  }
+    margin: '5% 25%',
+  },
 });
 
 export default () => {
@@ -45,11 +45,8 @@ export default () => {
         </List>
 
         <Typography variant="body1">
-          Remember to refer to our{" "}
-          <a href="https://react.eogresources.com/assessing">
-            How We Assess Submissions
-          </a>{" "}
-          guidelines.
+          Remember to refer to our <a href="https://react.eogresources.com/assessing">How We Assess Submissions</a>{' '}
+          guidelines, as well as the <a href="https://react.eogresources.com/api">GraphQL API Documentation</a>.
         </Typography>
       </CardContent>
     </Card>
