414989ae940613e8bf31d1c6a02c80d5be560555
saketh1991 <devinenisaketh@gmail.com>
2021-03-22 23:27:49 +0530

    WIP: setup basre repository 1. Clone and setup base repository. 2. Update username in the header  component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
 export default () => {
   const classes = useStyles();
 
-  const name = "$USERNAME's";
+  const name = "Saketh's";
   return (
     <AppBar position="static">
       <Toolbar>
