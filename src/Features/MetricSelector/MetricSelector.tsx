277987b4d149a13db49cf7619591a4cb4f127433
saketh1991 <devinenisaketh@gmail.com>
2021-03-23 21:55:37 +0530

    FEAT: add last known value Add alst known value card for the selected instruments.

--- src/Features/MetricSelector/MetricSelector.tsx
+++ src/Features/MetricSelector/MetricSelector.tsx
@@ -1,13 +1,32 @@
 import { useLazyQuery } from '@apollo/client';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
+import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { gql, Provider, useQuery } from 'urql';
 import Checkbox from '../../components/Checkbox';
 import { IState } from '../../store';
 import { client } from '../../utils/client';
-import { actions, Metric } from './reducer';
+import { actions, Measurement, Metric } from './reducer';
+
+const useStyles = makeStyles({
+  container: {
+    paddingTop: "20px",
+    alignItems: 'center',
+    display: "flex"
+  },
+  card: {
+    background: 'white',
+    padding: '10px',
+    borderRadius: '4px',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    margin: '10px',
+    height: 40
+  }
+});
+
 
 const getMetricsQuery = `
 query {
@@ -37,6 +56,7 @@ function MetricSelectorImpl(props: { client: any }) {
 
     const metrics = useSelector((state: IState) => state.metrics.instruments);
     const selectedMetrics: Metric[] = useSelector((state: IState) => state.metrics.instruments.filter(m => m.isChecked));
+    const measurementsData: { [key: string]: Measurement[] } = useSelector((state: IState) => state.metrics.measurements);
 
     let selectedMetricObject: { [key: string]: boolean; } = {};
 
@@ -84,10 +104,16 @@ function MetricSelectorImpl(props: { client: any }) {
         loadMeasurements({ variables: { input } })
     }
 
+    const classes = useStyles();
+
     return (
-        <FormControl component="fieldset">
+        <FormControl className={classes.container} component="fieldset">
             <FormGroup aria-label="position" row>
-                {metrics.map(metric => <Checkbox key={metric.value} metric={metric} handleChange={handleChange} />)}
+                {metrics.map((metric, index) => <div key={index}> 
+                    <Checkbox key={metric.value} metric={metric} handleChange={handleChange} />
+                    {measurementsData[metric.value]?.slice(-1)[0] && <div className={classes.card} key={`${metric.value}-card`}>{(measurementsData[metric.value]?.slice(-1)[0] as any)?.value}</div>}
+                </div>
+                )}
             </FormGroup>
         </FormControl>
     );
