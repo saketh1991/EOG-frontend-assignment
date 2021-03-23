665e94f1236a4ed6a0f04d7e89076f2b04fd437e
saketh1991 <devinenisaketh@gmail.com>
2021-03-23 22:27:01 +0530

    fix: chart styling Add styling to the chart component.

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,38 +1,62 @@
+import { makeStyles } from "@material-ui/core/styles";
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { Provider } from 'urql';
 import { Measurement } from '../Features/MetricSelector/reducer';
 import { IState } from '../store';
 import { client } from '../utils/client';
 import { colors } from '../utils/constants';
 
+const useStyles = makeStyles({
+  message: {
+    height: '80%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '22px',
+    color: '#969598'
+  }
+});
+
 interface Color {
   [key: string]: string;
 }
 
 function Chart() {
+
+  const classes = useStyles();
+
   const selectedMetricsMeasurements: { [key: string]: Measurement[] } = useSelector((state: IState) => state.metrics.measurements);
+  if (!Object.keys(selectedMetricsMeasurements).length) {
+    return <div className={classes.message}>
+      Please Select An Instrument.
+    </div>;
+  }
   return (
-    <LineChart width={1000} height={600}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="at" type="category" allowDuplicatedCategory={false} />
-      <YAxis dataKey="value" />
-      <Tooltip />
-      <Legend layout="vertical" verticalAlign="middle" align="right" />
-      {Object.keys(selectedMetricsMeasurements).map(instrument => {
-        return (
-          <Line
-            dataKey="value"
-            data={selectedMetricsMeasurements[instrument]}
-            name={instrument}
-            key={instrument}
-            dot={false}
-            stroke={(colors as Color)[instrument]} 
-          />
-        );
-      })}
-    </LineChart>
+    <ResponsiveContainer width="100%" height="70%">
+      <LineChart >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="at" type="category" allowDuplicatedCategory={false} label="Time" style={{margin: '10px'}} />
+        <YAxis dataKey="value" label="PSI" />
+        <Tooltip />
+        <Legend layout="vertical" verticalAlign="middle" align="right" />
+        {Object.keys(selectedMetricsMeasurements).map(instrument => {
+          return (
+            <Line
+              dataKey="value"
+              data={selectedMetricsMeasurements[instrument]}
+              name={instrument}
+              key={instrument}
+              dot={false}
+              stroke={(colors as Color)[instrument]}
+            />
+          );
+        })}
+      </LineChart>
+    </ResponsiveContainer>
+
   );
 }
 
